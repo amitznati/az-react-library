2561d3a3f9bcd0dbc93a89ff62e4db1467146500
Amit Znati <Amit.Znati@amdocs.com>
2024-01-18 19:46:05 +0200

    Initial commit

---
+++ src/components/Input/Input.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Input from "./Input";
+import {InputProps} from "./Input.types.ts";
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+  title: "Example/InputField",
+  argTypes: {},
+};
+export default meta;
+
+type Story = StoryObj<typeof Input>;
+
+export const Primary: Story = (args: InputProps) => (
+  <Input data-test-id="InputField-id" {...args} />
+);
+Primary.args = {
+  error: false,
+  disabled: false,
+  label: "Primary",
+};
+
+export const Success: Story = (args: InputProps) => (
+  <Input data-test-id="InputField-id" {...args} />
+);
+Success.args = {
+  error: false,
+  success: true,
+  disabled: false,
+  label: "Success",
+};
+
+export const Error: Story = (args: InputProps) => (
+  <Input data-test-id="InputField-id" {...args} />
+);
+Error.args = {
+  error: true,
+  disabled: false,
+  message: "Error",
+};
+
+export const Disabled: Story = (args: InputProps) => (
+  <Input data-test-id="InputField-id" {...args} />
+);
+Disabled.args = {
+  disabled: true,
+  label: "Disabled",
+};
